7a2b26ffc31cf177fac3893ab0c5d9984a4c32d2
David Ceja Zapata <d.ceja@ynemx.com>
2026-01-14 17:52:50 -0600

    View for display board data

---
+++ src/modules/boards/components/BoardView.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+</script>
+
+<template>
+  <h1>Board {{ route.params.boardId }}</h1>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,14 @@ import AccountHome from '@/modules/accountHome/components/AccountHome.vue'
 import ProjectsHome from '@/modules/projectsHome/components/ProjectsHome.vue'
 import ReportsHome from '@/modules/reportsHome/components/ReportsHome.vue'
 import TrackingHome from '@/modules/trackingHome/components/TrackingHome.vue'
+import BoardView from '@/modules/boards/components/BoardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/projects' },
     { path: '/projects', component: ProjectsHome },
+    { path: '/projects/boards/:boardId', component: BoardView },
     { path: '/account', component: AccountHome },
     { path: '/reports', component: ReportsHome },
     { path: '/tracking', component: TrackingHome },
